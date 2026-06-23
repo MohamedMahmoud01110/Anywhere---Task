3c2af770739000fdeea187b2b7f980a307d605cb
MohamedMahmoud01110 <mohamed.mahmoud14532@gmail.com>
2026-06-23 06:30:05 +0300

    update integration

--- Back end/app.js
+++ Back end/app.js
@@ -13,10 +13,12 @@ const app = express();
 app.use(express.json());
 
 // cors
-app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+    credentials: true,
+  }),
+);
 
 // security headers
 app.use(
--- Front end/src/api/Axios.tsx
+++ Front end/src/api/Axios.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "http://localhost:3000/api",
-  baseURL: "https://anywhere-task-production.up.railway.app/api",
+  baseURL: "http://localhost:3000/api",
+  // baseURL: "https://anywhere-task-production.up.railway.app/api",
 });
 
 export default API;
